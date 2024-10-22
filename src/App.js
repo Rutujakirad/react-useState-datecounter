a7b24db21b3d2583fa3b44761bb62bc41d841559
Rutuja Kirad <chinmayikirad31@gmail.com>
2024-10-22 12:22:41 +0530

    Initial commit of react-useState-datecounter project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App(){
+  return(
+    <div>
+<Counter />,
+<Step />
+</div>  
+);
+}
+
+function Step(){
+ const[step, setStep] = useState(1)
+ 
+return(
+<div className="button">
+ 
+<button onClick={() =>setStep((c) => c-step)}>-</button>
+      <span>step: {step}</span>
+      <button onClick={()=> setStep((c) =>c+step)}>+</button>
+</div>
   );
 }
 
-export default App;
+
+function Counter(){
+const[count, setCount]= useState(0)
+
+const date= new Date("july 03 2023");
+date.setDate(date.getDate()+count);
+
+  return <div>
+    <div className="button">
+      <button onClick={() =>setCount((c) => c-1)}>-</button>
+      <span>Count: {count}</span>
+      <button onClick={()=> setCount((c) =>c+1)}>+</button>
+    </div>
+
+<p>
+  <span>{count === 0 
+    ? "Today is  "  : count > 0 
+    ? `${count} data from today is`
+     :`${Math.abs(count)} days ago was`}
+    </span>
+   <span>{date.toDateString()}</span>
+ </p>
+ </div>
+}
+
